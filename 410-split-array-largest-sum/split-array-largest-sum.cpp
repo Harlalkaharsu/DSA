97927a07082aeb6b5f80fbe7905aa7d8bb8c0981
Harshit Harlalka <harshitharlalka8118@gmail.com>
2025-09-15 13:20:20 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (24.58%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -1,39 +1,32 @@
-#include <vector>
-#include <algorithm>
-using namespace std;
-
 class Solution {
+private:
+    int cnt(vector<int>& nums, int k){
+        int n = nums.size();
+        int parti = 1;
+        long long sum = 0;
+        for(int i = 0; i<n ; i++){
+            if(sum + nums[i] <=  k){
+                sum += nums[i];
+            }
+            else {
+                parti++;
+                sum = nums[i];
+            }
+        }
+        return parti;
+    }
 public:
     int splitArray(vector<int>& nums, int k) {
-        int low = 0, high = 0;
-        for (int num : nums) {
-            low = max(low, num);
-            high += num;
-        }
-        int ans = high;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (isFeasible(nums, mid, k)) {
-                ans = mid;
-                high = mid - 1;
-            } else {
-                low = mid + 1;
+        int low = *max_element(nums.begin(),nums.end());
+        int high = accumulate(nums.begin(),nums.end(), 0);
+        while(low<=high){
+            int mid = low +(high-low) / 2;
+            int parti = cnt(nums, mid);
+            if(parti <= k){
+                high = mid -1;
             }
+            else low = mid+1;
         }
-        return ans;
-    }
-    
-    bool isFeasible(vector<int>& nums, int maxSum, int k) {
-        int count = 1;
-        int currentSum = 0;
-        for (int num : nums) {
-            currentSum += num;
-            if (currentSum > maxSum) {
-                count++;
-                currentSum = num;
-                if (count > k) return false;
-            }
-        }
-        return true;
+        return low;
     }
 };
