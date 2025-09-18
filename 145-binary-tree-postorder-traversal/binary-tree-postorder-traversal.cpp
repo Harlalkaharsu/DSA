f6273096f0c9f39a310c3805307e4fff414fb995
Harshit Harlalka <harshitharlalka8118@gmail.com>
2025-09-18 11:21:24 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (8.11%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
@@ -16,10 +16,10 @@ public:
         postorder(root, ans);
         return ans;
     }
-    void postorder(TreeNode* root, vector<int>&ans){
-        if(root ==  nullptr) return;
-        postorder(root->left,ans);
-        postorder(root->right,ans);
+    void postorder(TreeNode* root,vector<int>&ans){
+        if(root == nullptr) return;
+        postorder(root->left, ans);
+        postorder(root->right, ans);
         ans.push_back(root->val);
     }
 };
