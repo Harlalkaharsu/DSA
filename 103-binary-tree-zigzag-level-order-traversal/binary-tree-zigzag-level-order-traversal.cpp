5e0f90648d54a4e8a581942f98d4ae684b15c5d1
Harshit Harlalka <harshitharlalka8118@gmail.com>
2025-09-21 02:03:57 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (76.23%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
@@ -1,35 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        vector<vector<int>> res;
-        if (!root) return res;
-
-        deque<TreeNode*> dq;
-        dq.push_back(root);
+        vector<vector<int>> ans;
+        if(!root) return ans;
+        deque<TreeNode*> q;
+        q.push_back(root);
         bool reverse = false;
-
-        while (!dq.empty()) {
-            int size = dq.size();
+        while(!q.empty()){
+            int size = q.size();
             vector<int> level;
-
-            for (int i = 0; i < size; i++) {
-                if (!reverse) {
-                    TreeNode* node = dq.front(); dq.pop_front();
-                    level.push_back(node->val);
-                    if (node->left) dq.push_back(node->left);
-                    if (node->right) dq.push_back(node->right);
-                } else {
-                    TreeNode* node = dq.back(); dq.pop_back();
-                    level.push_back(node->val);
-                    if (node->right) dq.push_front(node->right);
-                    if (node->left) dq.push_front(node->left);
+            for(int i = 0; i< size; i++){
+                if(!reverse){
+                    TreeNode* root = q.front(); q.pop_front();
+                    level.push_back(root->val);
+                    if(root->left) q.push_back(root->left);
+                    if(root->right) q.push_back(root->right);
+                }else{
+                    TreeNode* root = q.back(); q.pop_back();
+                    if(root->right) q.push_front(root->right);
+                    if(root->left) q.push_front(root->left);
+                    level.push_back(root->val);
                 }
             }
-
-            res.push_back(level);
+            ans.push_back(level);
             reverse = !reverse;
         }
-
-        return res;
+        return ans;
     }
 };
