391c9df8e5174afe52f5148068fd8ed825006a8c
Harshit Harlalka <harshitharlalka8118@gmail.com>
2025-09-23 15:18:43 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (11.54%) - LeetSync

--- 1965-sum-of-digits-in-base-k/sum-of-digits-in-base-k.cpp
+++ 1965-sum-of-digits-in-base-k/sum-of-digits-in-base-k.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     int sumBase(int n, int k) {
-        return n == 0?0:n%k+sumBase(n/k, k);
+        int answer = 0;
+        // Convert n to base k and sum each digit
+        while (n > 0) {
+            answer += n % k; // Add the current digit (n mod k)
+            n /= k; // Remove the processed digit
+        }
+        return answer;
     }
 };
