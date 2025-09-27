29a94a6412e14cbfd713db2378768c7196110bdd
Harshit Harlalka <harshitharlalka8118@gmail.com>
2025-09-27 14:05:33 +0530

    Time: 747 ms (36.50%) | Memory: 11.6 MB (33.69%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,28 +1,36 @@
 class Solution {
 public:
     bool exist(vector<vector<char>>& board, string word) {
-        int n = board.size();
-        int m = board[0].size();
-        int index =0;
-        for(int i = 0; i<n; i++){
-            for(int j =0 ; j<m; j++){
-                if(board[i][j] == word[index]){
-                    if(searchnext(board, word, n,m,index,i,j)) return true;
+        int m = board.size();
+        int n = board[0].size();
+        
+        function<bool(int, int, int)> backtrack = [&](int i, int j, int k) {
+            if (k == word.length()) {
+                return true;
+            }
+            if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] != word[k]) {
+                return false;
+            }
+            
+            char temp = board[i][j];
+            board[i][j] = '\0';
+            
+            if (backtrack(i + 1, j, k + 1) || backtrack(i - 1, j, k + 1) || 
+                backtrack(i, j + 1, k + 1) || backtrack(i, j - 1, k + 1)) {
+                return true;
+            }
+            
+            board[i][j] = temp; 
+            return false;
+        };
+        
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (backtrack(i, j, 0)) {
+                    return true;
                 }
             }
         }
         return false;
-    }
-    bool searchnext(vector<vector<char>>& board,string word, int n, int m, int index, int i, int j){
-        if(index == word.length()) return true;
-        if(i <0 || j<0 || i == n||j == m || board[i][j] != word[index] || board[i][j] == '!') return false;
-        char c = board[i][j];
-        board[i][j] = '!';
-        bool top = searchnext(board, word,n,m,index+1,i-1,j);
-        bool right = searchnext(board, word,n,m,index+1,i,j-1);
-        bool bottom = searchnext(board, word,n,m,index+1,i+1,j);
-        bool left = searchnext(board, word,n,m,index+1,i,j+1);
-        board[i][j] = c;
-        return top || right || bottom || left;
     }
 };
