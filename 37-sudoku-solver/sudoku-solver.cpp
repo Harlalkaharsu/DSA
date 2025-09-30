82a993da4900b35c516d2b513dd73700cedb71b6
Harshit Harlalka <harshitharlalka8118@gmail.com>
2025-09-30 22:03:32 +0530

    Time: 373 ms (43.13%) | Memory: 8.8 MB (65.16%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.cpp
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -26,7 +26,7 @@ public:
         for (int i = 0; i < 9; i++) {
             if (board[row][i] == c) return false;
             if (board[i][col] == c) return false;
-            if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c) return false;
+            if (board[3 * (row / 3) + (i / 3)][3 * (col / 3) + i % 3] == c) return false;
         }
         return true;
     }
