9dcaf0e56b22cca59788cb764f184c11c9c615c4
Harshit Harlalka <harshitharlalka8118@gmail.com>
2025-10-06 21:08:18 +0530

    Time: 257 ms (27.57%) | Memory: 342.3 MB (10.64%) - LeetSync

--- 808-number-of-matching-subsequences/number-of-matching-subsequences.cpp
+++ 808-number-of-matching-subsequences/number-of-matching-subsequences.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
-
-    bool issubsequence(string& s, string t){
-        int i = 0, j = 0, n = s.size(), m = t.size();
-
-        while(i<n and j<m){
-            if(s[i] == t[j]) j++;
-            i++;
+    bool isreal(string s, string t){
+        int i =0, j=0, n=s.size(), m=t.size();
+        while(i<n && j<m){
+            if(s[i] == t[j])j++;i++;
         }
-
-        return j == m;
+        return j==m;
     }
-
     int numMatchingSubseq(string s, vector<string>& words) {
-        int ans = 0;
-        unordered_map<string, int> mp;
-        for(auto i : words) mp[i]++;
-
-        for(auto i : mp) if(issubsequence(s, i.first)) ans += i.second;
-
-        return ans;  
+        int cnt=0;
+        unordered_map<string,int> mp;
+        for(auto i:words)mp[i]++;
+        for(auto i:mp){
+            if(isreal(s,i.first)) cnt += i.second;
+        }
+        return cnt;
     }
 };
