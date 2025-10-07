c05613939a6069e6974e737981674c86abdbd6a6
Harshit Harlalka <harshitharlalka8118@gmail.com>
2025-10-07 13:03:43 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (46.04%) - LeetSync

--- 38-count-and-say/count-and-say.cpp
+++ 38-count-and-say/count-and-say.cpp
@@ -7,7 +7,7 @@ public:
     string make(const string& s){
         string result;
         int count= 1;
-        for(int i =1; i<s.length(); i++){
+        for(int i =1; i<s.size(); i++){
             if(s[i] ==s[i-1]) count++;
             else{
                 result += to_string(count) + s[i-1];
