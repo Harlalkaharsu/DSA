be006622472c720d7f18fcf18b0fe4dcaa0dd29a
Harshit Harlalka <harshitharlalka8118@gmail.com>
2025-10-07 12:06:18 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (86.68%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.cpp
+++ 165-compare-version-numbers/compare-version-numbers.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    static int compareVersion(string& v1, string& v2) {
-        const int n1=v1.size(), n2=v2.size();
+    int compareVersion(string v1, string v2) {
+        int n= v1.size(), m = v2.size();
         int x1=0, x2=0;
-        for(int i=0, j=0; i<n1 || j<n2; i++, j++){
-            while(i<n1 && v1[i]!='.'){
-                x1=10*x1+(v1[i++]-'0');
+        for(int i=0, j=0; i<n || j<m; i++,j++){
+            while(i<n && v1[i]!='.'){
+                x1 =10*x1 + (v1[i++] - '0');
             }
-            while(j<n2 && v2[j]!='.'){
-                x2=10*x2+(v2[j++]-'0');
+            while(j<m && v2[j]!='.'){
+                x2 =10*x2 + (v2[j++] - '0');
             }
-            if (x1<x2) return -1;
+            if(x1<x2) return -1;
             else if (x1>x2) return 1;
             x1=0;
             x2=0;
