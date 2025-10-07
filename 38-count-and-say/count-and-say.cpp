6619110dbeb25a18ef3fc1da4d0415a307c86ef4
Harshit Harlalka <harshitharlalka8118@gmail.com>
2025-10-07 12:55:28 +0530

    Time: 5 ms (35.07%) | Memory: 10.2 MB (30.28%) - LeetSync

---
+++ 38-count-and-say/count-and-say.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        if(n==1) return "1";
+        return make(countAndSay(n-1));
+    }
+    string make(const string& s){
+        string result;
+        int count= 1;
+        for(int i =1; i<s.length(); i++){
+            if(s[i] ==s[i-1]) count++;
+            else{
+                result += to_string(count) + s[i-1];
+                count=1;
+            }
+        }
+        result+=to_string(count) + s.back();
+        return result;
+    }
+};
