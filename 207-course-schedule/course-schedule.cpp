3a4b59ea3df709f9169b7b9056d713ae4987c40d
Harshit Harlalka <harshitharlalka8118@gmail.com>
2025-10-08 17:12:25 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (54.44%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,38 +1,42 @@
 class Solution {
-private:
-    bool dfs(int i, vector<vector<int>>& adj, unordered_set<int>& set, vector<bool>& visited) {
-        set.insert(i);
-        visited[i] = true;
-
-        for (int num : adj[i]) {
-            if (!visited[num]) {
-                if (!dfs(num, adj, set, visited))
-                    return false;
-            } else if (set.count(num)) {
-                return false;
-            }
-        }
-
-        set.erase(i);
-        return true;
-    }
-
 public:
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> adj(numCourses);
-        for (auto& edge : prerequisites) {
-            adj[edge[1]].push_back(edge[0]);
+
+    bool canFinish(int V, vector<vector<int>>& arr) {
+        if(arr.size() == 0)   return true;
+
+        list<int> *l;
+        l = new list<int>[V];
+
+        for(int i=0; i<arr.size(); i++){
+            l[arr[i][1]].push_back(arr[i][0]);
         }
 
-        vector<bool> visited(numCourses, false);
-        for (int i = 0; i < numCourses; ++i) {
-            if (!visited[i]) {
-                unordered_set<int> set;
-                if (!dfs(i, adj, set, visited))
-                    return false;
+        vector<int> indegree(V, 0);
+
+        for(int i=0; i<V; i++){
+            for(int j : l[i]){
+                indegree[j]++;
             }
         }
 
-        return true;
+        queue<int> q;
+
+        for(int i=0; i<indegree.size(); i++){
+            if(indegree[i] == 0)    q.push(i);
+        }
+
+        int cnt = 0;
+        while(q.size() > 0){
+            int node = q.front();
+            q.pop();
+            cnt++;
+
+            for(int i : l[node]){
+                indegree[i]--;
+                if(indegree[i] == 0) q.push(i);
+            }
+        }
+
+        return cnt == V;
     }
 };
