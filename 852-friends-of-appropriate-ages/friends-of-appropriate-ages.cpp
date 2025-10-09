03a7756fc78fd5fdf6b6ad2cf5e1baa3f9ea14c9
Harshit Harlalka <harshitharlalka8118@gmail.com>
2025-10-09 11:47:06 +0530

    Time: 8 ms (53.61%) | Memory: 41.1 MB (82.77%) - LeetSync

--- 852-friends-of-appropriate-ages/friends-of-appropriate-ages.cpp
+++ 852-friends-of-appropriate-ages/friends-of-appropriate-ages.cpp
@@ -1,29 +1,16 @@
 class Solution {
 public:
     int numFriendRequests(vector<int>& ages) {
-        sort(ages.begin(), ages.end());
         int n = ages.size();
-        int ans = 0;
-
-        int left = 0, right = 0;
-
-        for (int i = 0; i < n; i++) {
+        sort(ages.begin(), ages.end());
+        int ans=0, left=0, right=0;
+        for(int i=0; i<n;i++){
             int age = ages[i];
-            if (age < 15) continue;  // below 15 can't send requests
-
-            // move left pointer until it satisfies: ages[left] > 0.5 * age + 7
-            while (left < n && ages[left] <= 0.5 * age + 7)
-                left++;
-
-            // move right pointer while next person is still <= age
-            while (right + 1 < n && ages[right + 1] <= age)
-                right++;
-
-            // count how many are in (left, right]
-            ans += right - left;
-
+            if(age < 15)continue;
+            while(left <n && ages[left] <= 0.5*age + 7) left++;
+            while(right+1 <n && ages[right+1] <= age) right++;
+            ans += right-left;
         }
-
         return ans;
     }
 };
