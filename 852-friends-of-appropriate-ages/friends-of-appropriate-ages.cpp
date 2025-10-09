de1ddca4d68b0e0e72267d7b296c2acd5cb57363
Harshit Harlalka <harshitharlalka8118@gmail.com>
2025-10-09 11:52:46 +0530

    Time: 11 ms (49.34%) | Memory: 41.3 MB (17.38%) - LeetSync

--- 852-friends-of-appropriate-ages/friends-of-appropriate-ages.cpp
+++ 852-friends-of-appropriate-ages/friends-of-appropriate-ages.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int numFriendRequests(vector<int>& ages) {
-        int n = ages.size();
+        int n =ages.size();
         sort(ages.begin(), ages.end());
         int ans=0, left=0, right=0;
-        for(int i=0; i<n;i++){
-            int age = ages[i];
-            if(age < 15)continue;
-            while(left <n && ages[left] <= 0.5*age + 7) left++;
-            while(right+1 <n && ages[right+1] <= age) right++;
+        for(int i =0; i<n; i++){
+            int age= ages[i];
+            if(age<15) continue;
+            while(left< n && ages[left] <= 0.5 *age+7)left++;
+            while(right+1<n && ages[right+1] <= age) right++;
             ans += right-left;
         }
         return ans;
