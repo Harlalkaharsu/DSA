0e989e4501be57c2e510a346b5aec4cd8f0365a6
Harshit Harlalka <harshitharlalka8118@gmail.com>
2025-10-09 14:36:23 +0530

    Time: 26 ms (51.57%) | Memory: 22.4 MB (89.87%) - LeetSync

--- 720-longest-word-in-dictionary/longest-word-in-dictionary.cpp
+++ 720-longest-word-in-dictionary/longest-word-in-dictionary.cpp
@@ -5,7 +5,7 @@ public:
         unordered_set<string>res;
         res.insert("");
         string ans="";
-        for(auto w:words){
+        for(auto &w:words){
             if(res.count(w.substr(0,w.size()-1))){
                 res.insert(w);
                 if(w.size()>ans.size()){
