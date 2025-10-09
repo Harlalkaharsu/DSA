02e97acfa4bbfab4a2dc28850f9b15b7a5db9391
Harshit Harlalka <harshitharlalka8118@gmail.com>
2025-10-09 14:36:05 +0530

    Time: 35 ms (33.30%) | Memory: 23.7 MB (67.75%) - LeetSync

---
+++ 720-longest-word-in-dictionary/longest-word-in-dictionary.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string longestWord(vector<string>& words) {
+        sort(words.begin(),words.end());
+        unordered_set<string>res;
+        res.insert("");
+        string ans="";
+        for(auto w:words){
+            if(res.count(w.substr(0,w.size()-1))){
+                res.insert(w);
+                if(w.size()>ans.size()){
+                    ans=w;
+                }
+            }
+        }
+        return ans;
+    }
+};
