344252dfc6b18f63de633806169e8402955f78d8
Harshit Harlalka <harshitharlalka8118@gmail.com>
2025-10-15 10:28:48 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (11.87%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        map<int, int> mp;
+        for(int x: nums){
+            mp[x]++;
+        }
+        int index = 0;
+        for (auto it : mp) {        
+            int value = it.first;
+            int count = it.second;
+            while (count--) {
+                nums[index++] = value;
+            }
+        }
     }
 };
