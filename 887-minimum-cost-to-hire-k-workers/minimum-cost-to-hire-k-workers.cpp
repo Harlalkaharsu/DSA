9c22b823d5816ef71ee781f9dae6152c941bc182
Harshit Harlalka <harshitharlalka8118@gmail.com>
2025-10-30 12:10:23 +0530

    Time: 21 ms (15.63%) | Memory: 32.3 MB (14.99%) - LeetSync

---
+++ 887-minimum-cost-to-hire-k-workers/minimum-cost-to-hire-k-workers.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    double mincostToHireWorkers(vector<int>& quality, vector<int>& wage, int k) {
+        vector<pair<double,int>> ratio;
+        priority_queue<pair<int,int>> pq;
+        double ans = DBL_MAX, q_sum = 0;
+       
+        for(int i=0; i<quality.size(); i++) {
+            ratio.push_back({(double)wage[i]/quality[i], i});
+        }
+        sort(ratio.begin(),ratio.end());
+
+         for(int i=0; i<wage.size(); i++) {
+            int index = ratio[i].second;
+            q_sum += quality[index]; //add quality of current worker
+            pq.push({quality[index],index});
+
+            if(pq.size() > k) { //do not store more than k workers in pq
+                q_sum -= pq.top().first;
+                pq.pop();
+            }
+        //total wage of k workers = (sum of quality) * (highest ratio)
+            if(pq.size() == k) { 
+                ans = min(ans,q_sum * ratio[i].first); //ratio is already sorted, so just multiply by current ratio
+            }
+        }
+     return ans;
+    }
+};
