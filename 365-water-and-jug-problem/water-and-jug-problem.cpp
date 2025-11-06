c9f60de7a3f0031e60a0dce4281e4bbd53c8fdb3
Harshit Harlalka <harshitharlalka8118@gmail.com>
2025-11-06 09:34:10 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (47.93%) - LeetSync

--- 365-water-and-jug-problem/water-and-jug-problem.cpp
+++ 365-water-and-jug-problem/water-and-jug-problem.cpp
@@ -1,44 +1,39 @@
 class Solution {
 public:
-    bool canMeasureWater(int jug1Capacity, int jug2Capacity, int targetCapacity) {
-        int t;
-        queue<pair<int, int>> q;
-        map<pair<int, int>, bool> visited;
-        // BFS
-        q.push({0, 0});
-        while(q.size()) {
-            auto [jug1, jug2] = q.front();
-            q.pop();
-            visited[{jug1, jug2}] = true;
-            if(jug1 == targetCapacity || jug2 == targetCapacity || jug1 + jug2 == targetCapacity)
-                return true;
-            // Fill any of the jugs
-            if(!visited[{jug1Capacity, jug2}])
-                q.push({jug1Capacity, jug2});
-            if(!visited[{jug1, jug2Capacity}])
-                q.push({jug1, jug2Capacity});
-            // Empty any of the jugs
-            if(!visited[{0, jug2}])
-                q.push({0, jug2});
-            if(!visited[{jug1, 0}])
-                q.push({jug1, 0});
-            // Pour water into other
-            t = min(jug1Capacity, jug1 + min(jug2Capacity, jug2));
-            if(!visited[{t, jug1 + jug2 - t}])
-                q.push({t, jug1 + jug2 - t});
-            t = min(jug2Capacity, jug2 + min(jug1Capacity, jug1));
-            if(!visited[{jug1 + jug2 - t, t}])
-                q.push({jug1 + jug2 - t, t});
+    bool canMeasureWater(int x, int y, int target) {
+        // (a)x + (b)y = (c)gcd(x, y)
+        // or BFS
+        // add x ; add y ; subtract x ; subtract y
+        int mx = x+y;
+        vector<bool>vis(1000, false);
+        queue<int>Q;
+        Q.push(0);
+        vis[0] = true;
+
+        while(!Q.empty()){
+            int e = Q.front(); 
+            Q.pop();
+            if(e == target) return true;
+
+            if(e+x <= mx && !vis[e+x]){
+                Q.push(e+x);
+                vis[e+x] = true;
+            }
+            if(e+y <= mx && !vis[e+y]){
+                Q.push(e+y);
+                vis[e+y] = true;
+            }
+            if(e-x >= 0 && !vis[e-x]){
+                Q.push(e-x);
+                vis[e-x] = true;
+            }
+            if(e-y >= 0 && !vis[e-y]){
+                Q.push(e-y);
+                vis[e-y] = true;
+            }
         }
         return false;
+
+
     }
 };
-
-/* Operations 
-    * jug1 = jug1Capacity
-    * jug2 = jug2Capacity
-    * jug1 = 0
-    * jug2 = 0
-    * jug1 = min(jug1Capacity, jug1 + min(jug2Capacity, jug2))
-    * jug2 = min(jug2Capacity, jug2 + min(jug1Capacity, jug1))
-*/
