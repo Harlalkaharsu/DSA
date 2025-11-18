ab727bec77136385af26d8eb3a750c032782d448
Harshit Harlalka <harshitharlalka8118@gmail.com>
2025-11-18 11:19:19 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (39.86%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -3,14 +3,14 @@ public:
     int characterReplacement(string s, int k) {
         int left = 0, maxLen = 0, n = s.size(), maxCount = 0;
         int hash[26] = {0};
-        for(int right = 0; right < n; right++){
-            hash[s[right] - 'A']++;
-            maxCount = max(maxCount, hash[s[right] - 'A']);
-            if((right - left + 1) - maxCount> k){
+        for(int i = 0; i < n; i++){
+            hash[s[i] - 'A']++;
+            maxCount = max(maxCount, hash[s[i] - 'A']);
+            if((i - left + 1) - maxCount> k){
                 hash[s[left] - 'A']--;
                 left++;
             }
-            maxLen = max(maxLen, right - left + 1);
+            maxLen = max(maxLen, i - left + 1);
         }
         return maxLen;
     }
