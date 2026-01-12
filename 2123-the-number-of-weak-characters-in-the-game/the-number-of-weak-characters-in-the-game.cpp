cfc32fc222d06b71f3d0fd4dc5750a6010ab6bd3
Harshit Harlalka <harshitharlalka8118@gmail.com>
2026-01-12 14:46:27 +0530

    Time: 86 ms (50.38%) | Memory: 129.4 MB (46.80%) - LeetSync

--- 2123-the-number-of-weak-characters-in-the-game/the-number-of-weak-characters-in-the-game.cpp
+++ 2123-the-number-of-weak-characters-in-the-game/the-number-of-weak-characters-in-the-game.cpp
@@ -1,16 +1,28 @@
 class Solution {
 public:
     int numberOfWeakCharacters(vector<vector<int>>& properties) {
-        sort(properties.begin(), properties.end(), [](vector<int>& x, vector<int>& y){
-            if(x[0] == y[0]){
-                return (x[1]<y[1]);
-            }
-            return x[0]>y[0];
+       sort(properties.begin(), properties.end(), [](const vector<int>& x, const vector<int>& y) {
+            
+           if(x[0]==y[0])
+           {
+            return x[1]<y[1];
+           }
+           return x[0]>y[0];
         });
-        int defense =0, count = 0;
-        for(int i = 0 ; i<properties.size(); i++){
-            if(properties[i][1]<defense) count++;
-            else defense = properties[i][1];
+        
+        int defense=0;
+        int count=0;
+        for(int i=0;i<properties.size();i++)
+        {
+          if(properties[i][1]<defense)
+          {
+            count++;
+          }
+          else 
+          {
+        
+        defense=properties[i][1]; 
+          }
         }
         return count;
     }
