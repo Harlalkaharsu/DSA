ec1098d73221f47d12c9a3dc8b0020d240226477
Harshit Harlalka <harshitharlalka8118@gmail.com>
2026-01-12 13:46:55 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (43.72%) - LeetSync

--- 768-partition-labels/partition-labels.cpp
+++ 768-partition-labels/partition-labels.cpp
@@ -1,17 +1,34 @@
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-        int start = 0, end = 0;
+        // Brute force two pointer
+        // int start = 0, end = 0;
+        // vector<int> ans;
+        // for(int i= 0; i<s.size(); i++){
+        //     if(i>end) {ans.push_back(end-start+1);
+        //     start = i;
+        //     end = i;}
+        //     for(int j = i; j<s.size(); j++){
+        //         if(s[i]==s[j]&&j>end){
+        //             end = j;
+        //         }
+        //     }
+        // }
+        // ans.push_back(s.size()-start);
+        // return ans;
+        unordered_map<char,int> mp;
+        for(int i = 0; i<s.size(); i++){
+            mp[s[i]] = i;
+        }
         vector<int> ans;
-        for(int i= 0; i<s.size(); i++){
-            if(i>end) {ans.push_back(end-start+1);
-            start = i;
-            end = i;}
-            for(int j = i; j<s.size(); j++){
-                if(s[i]==s[j]&&j>end){
-                    end = j;
-                }
+        int start = 0, end = 0;
+        for(int i = 0; i<s.size(); i++){
+            if(i>end){
+                ans.push_back(end-start+1);
+                start = i;
+                end = i;
             }
+            end = max(end,mp[s[i]]);
         }
         ans.push_back(s.size()-start);
         return ans;
