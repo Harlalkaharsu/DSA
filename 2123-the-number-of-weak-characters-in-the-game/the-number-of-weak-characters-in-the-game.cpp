41a7bba8bcd713fd834078422ce1945e2e352670
Harshit Harlalka <harshitharlalka8118@gmail.com>
2026-01-12 14:44:33 +0530

    Time: 80 ms (66.24%) | Memory: 129.2 MB (68.29%) - LeetSync

---
+++ 2123-the-number-of-weak-characters-in-the-game/the-number-of-weak-characters-in-the-game.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numberOfWeakCharacters(vector<vector<int>>& properties) {
+        sort(properties.begin(), properties.end(), [](vector<int>& x, vector<int>& y){
+            if(x[0] == y[0]){
+                return (x[1]<y[1]);
+            }
+            return x[0]>y[0];
+        });
+        int defense =0, count = 0;
+        for(int i = 0 ; i<properties.size(); i++){
+            if(properties[i][1]<defense) count++;
+            else defense = properties[i][1];
+        }
+        return count;
+    }
+};
