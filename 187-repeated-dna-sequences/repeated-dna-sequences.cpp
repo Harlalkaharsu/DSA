d245c2cf8097713a100ca52e3754c8f62f11218c
Harshit Harlalka <harshitharlalka8118@gmail.com>
2026-01-14 15:05:48 +0530

    Time: 119 ms (6.51%) | Memory: 25.1 MB (57.28%) - LeetSync

---
+++ 187-repeated-dna-sequences/repeated-dna-sequences.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<string> findRepeatedDnaSequences(string s) {
+        int n = s.size();
+        vector<string> ans;
+        map<string, int> mpp;
+
+        for(int i = 0; i<n-9; i++){
+            string temp = "";
+            for(int j =i; j<i+10; j++){
+                temp.push_back(s[j]);
+            }
+            mpp[temp]++;
+        }
+        for(auto x: mpp){
+            if(x.second > 1){
+                ans.push_back(x.first);
+            }
+        }
+        return ans;
+    }
+};
