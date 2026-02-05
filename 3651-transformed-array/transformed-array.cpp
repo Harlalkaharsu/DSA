5272e383b25af97b553fa00e9dad35fbe39b8f8d
Harshit Harlalka <harshitharlalka8118@gmail.com>
2026-02-05 14:41:32 +0530

    Time: 3 ms (89.69%) | Memory: 25.8 MB (39.55%) - LeetSync

--- 3651-transformed-array/transformed-array.cpp
+++ 3651-transformed-array/transformed-array.cpp
@@ -5,11 +5,26 @@ public:
         vector<int> result(n);
 
         for (int i = 0; i < n; i++) {
-            // Formula to handle circular movement for both + and - values
-            int targetIndex = (i + nums[i] % n + n) % n;
-            result[i] = nums[targetIndex];
+            if (nums[i] == 0) {
+                result[i] = nums[i];
+            } else {
+                int steps = abs(nums[i]);
+                int pos = i;
+
+                if (nums[i] > 0) {
+                    for (int j = 0; j < steps; j++) {
+                        pos = (pos + 1) % n;
+                    }
+                } else {
+                    for (int j = 0; j < steps; j++) {
+                        pos = (pos - 1 + n) % n;
+                    }
+                }
+
+                result[i] = nums[pos];
+            }
         }
-        
+
         return result;
     }
 };
