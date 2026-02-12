6e7997efe1f60ffe463bc5fb2bc651d0cf5faf16
Harshit Harlalka <harshitharlalka8118@gmail.com>
2026-02-12 11:01:02 +0530

    Time: 44 ms (96.34%) | Memory: 25.5 MB (52.24%) - LeetSync

---
+++ 4055-longest-balanced-substring-i/longest-balanced-substring-i.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int longestBalanced(string s) {
+        int n = s.length();
+        int maxLen = 0;
+        for (int i = 0; i < n; ++i) {
+            vector<int> freq(26, 0);
+            int distinctCount = 0;
+            int maxFreq = 0;
+            for (int j = i; j < n; ++j) {
+                int charIdx = s[j] - 'a';
+                if (freq[charIdx] == 0) distinctCount++;
+                freq[charIdx]++;
+                maxFreq = max(maxFreq, freq[charIdx]);
+                int currentLen = j - i + 1;
+                if (distinctCount * maxFreq == currentLen) {
+                    maxLen = max(maxLen, currentLen);
+                }
+            }
+        }
+        return maxLen;
+    }
+};
