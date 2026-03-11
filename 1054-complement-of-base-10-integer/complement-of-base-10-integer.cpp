f26b76dbb5fe0ac448f330896f020c4dcbdbf926
Harshit Harlalka <harshitharlalka8118@gmail.com>
2026-03-11 09:46:04 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (4.02%) - LeetSync

--- 1054-complement-of-base-10-integer/complement-of-base-10-integer.cpp
+++ 1054-complement-of-base-10-integer/complement-of-base-10-integer.cpp
@@ -1,7 +1,12 @@
 class Solution {
 public:
-    int bitwiseComplement(unsigned n) {
-        if (n == 0) return 1;
-        return ~n & (1 << bit_width(n)) - 1;
+    int bitwiseComplement(int n) {
+
+        if(n==0) return 1;
+
+        int bits = log2(n) + 1;
+        int mask = (1<<bits) - 1;
+
+        return n ^ mask;
     }
 };
