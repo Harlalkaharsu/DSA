bb0bcc19cc14e2a59f6970da1a405d7e7a6b729e
Harshit Harlalka <harshitharlalka8118@gmail.com>
2026-03-19 12:49:49 +0530

    Time: 29 ms (90.24%) | Memory: 101.1 MB (89.43%) - LeetSync

--- 3492-count-submatrices-with-equal-frequency-of-x-and-y/count-submatrices-with-equal-frequency-of-x-and-y.cpp
+++ 3492-count-submatrices-with-equal-frequency-of-x-and-y/count-submatrices-with-equal-frequency-of-x-and-y.cpp
@@ -1,36 +1,23 @@
 class Solution {
 public:
     int numberOfSubmatrices(vector<vector<char>>& grid) {
-        int m = grid.size(), n = grid[0].size();
-        int ans = 0;
+        int rows = grid.size(), cols = grid[0].size();
+        vector<int> sumX(cols, 0);
+        vector<int> sumY(cols, 0);
+        int res = 0;
 
-        vector<vector<int>> px(m, vector<int>(n, 0));
-        vector<vector<int>> py(m, vector<int>(n, 0));
+        for (int i = 0; i < rows; i++) {
+            int rx = 0, ry = 0;
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                px[i][j] = (grid[i][j] == 'X');
-                py[i][j] = (grid[i][j] == 'Y');
-
-                if (i > 0) {
-                    px[i][j] += px[i - 1][j];
-                    py[i][j] += py[i - 1][j];
-                }
-                if (j > 0) {
-                    px[i][j] += px[i][j - 1];
-                    py[i][j] += py[i][j - 1];
-                }
-                if (i > 0 && j > 0) {
-                    px[i][j] -= px[i - 1][j - 1];
-                    py[i][j] -= py[i - 1][j - 1];
-                }
-
-                if (px[i][j] == py[i][j] && px[i][j] > 0) {
-                    ans++;
-                }
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == 'X') rx++;
+                else if (grid[i][j] == 'Y') ry++;
+                sumX[j] += rx;
+                sumY[j] += ry;
+                if (sumX[j] > 0 && sumX[j] == sumY[j]) res++;
             }
         }
 
-        return ans;
+        return res;
     }
 };
