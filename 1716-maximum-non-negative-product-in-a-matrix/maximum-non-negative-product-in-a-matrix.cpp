a7383c9eac50d778a50140f6e25880a94383eb64
Harshit Harlalka <harshitharlalka8118@gmail.com>
2026-03-23 11:08:47 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (74.44%) - LeetSync

--- 1716-maximum-non-negative-product-in-a-matrix/maximum-non-negative-product-in-a-matrix.cpp
+++ 1716-maximum-non-negative-product-in-a-matrix/maximum-non-negative-product-in-a-matrix.cpp
@@ -1,38 +1,37 @@
 class Solution {
 public:
     int maxProductPath(vector<vector<int>>& grid) {
-        int m = grid.size(), n = grid[0].size();
+        int n = grid.size(), m = grid[0].size();
         const long long MOD = 1e9 + 7;
 
-        vector<vector<long long>> maxProd(m, vector<long long>(n));
-        vector<vector<long long>> minProd(m, vector<long long>(n));
+        vector<vector<long long>> maxpro(n,vector<long long>(m));
+        vector<vector<long long>> minpro(n,vector<long long>(m));
 
-        maxProd[0][0] = minProd[0][0] = grid[0][0];
+        maxpro[0][0] = minpro[0][0] = grid[0][0];
 
-        for (int i = 1; i < m; i++) {
-            maxProd[i][0] = minProd[i][0] = maxProd[i - 1][0] * grid[i][0];
+        for(int i = 1; i<n; i++){
+            maxpro[i][0] = minpro[i][0] = grid[i][0] * maxpro[i-1][0];
+        }
+        for(int j = 1; j<m; j++){
+            maxpro[0][j] = minpro[0][j] = grid[0][j] * maxpro[0][j-1];
         }
 
-        for (int j = 1; j < n; j++) {
-            maxProd[0][j] = minProd[0][j] = maxProd[0][j - 1] * grid[0][j];
-        }
 
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
+        for (int i =1; i<n; i++){
+            for(int j =1; j<m; j++){
                 long val = grid[i][j];
 
-                long a = maxProd[i-1][j] * val;
-                long b = minProd[i-1][j] * val;
-                long c = maxProd[i][j-1] * val;
-                long d = minProd[i][j-1] * val;
+                long a = maxpro[i-1][j] * val;
+                long b = minpro[i-1][j] * val;
+                long c = maxpro[i][j-1] * val;
+                long d = minpro[i][j-1] * val;
 
-                maxProd[i][j] = max({a,b,c,d});
-                minProd[i][j] = min({a,b,c,d});
+                maxpro[i][j] = max({a,b,c,d});
+                minpro[i][j] = min({a,b,c,d});
             }
         }
-
-        long ans = maxProd[m-1][n-1];
-        if(ans < 0) return -1;
+        long ans =maxpro[n-1][m-1];
+        if(ans<0) return -1;
         return ans % MOD;
     }
 };
