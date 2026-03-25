00ce9da95e532c37bd31a41c65157de564341f57
Harshit Harlalka <harshitharlalka8118@gmail.com>
2026-03-25 11:25:53 +0530

    Time: 12 ms (45.68%) | Memory: 130.2 MB (53.24%) - LeetSync

--- 3849-equal-sum-grid-partition-i/equal-sum-grid-partition-i.cpp
+++ 3849-equal-sum-grid-partition-i/equal-sum-grid-partition-i.cpp
@@ -2,28 +2,37 @@ class Solution {
 public:
     bool canPartitionGrid(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
+
+        vector<long long> rowSum(m, 0), colSum(n, 0);
         long long total = 0;
-        
-        for (auto &row : grid)
-            for (int x : row)
-                total += x;
-        
-        if (total % 2) return false;
-        
-        long long target = total / 2, sum = 0;
-        
-        for (int i = 0; i < m - 1; i++) {
-            for (int j = 0; j < n; j++)
-                sum += grid[i][j];
-            if (sum == target) return true;
+
+        // Compute rowSum, colSum, total
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                rowSum[i] += grid[i][j];
+                colSum[j] += grid[i][j];
+                total += grid[i][j];
+            }
         }
-        
-        sum = 0;
-        
-        for (int j = 0; j < n - 1; j++) {
-            for (int i = 0; i < m; i++)
-                sum += grid[i][j];
-            if (sum == target) return true;
+
+        if (total % 2) return false;
+
+        if (check(rowSum, total)) return true;
+
+        if (check(colSum, total)) return true;
+
+        return false;
+    }
+
+    bool check(vector<long long>& arr, long long total) {
+        long long left = arr[0];
+        long long right = total - left;
+
+        for (int i = 1; i < arr.size(); i++) {
+            if (left == right) return true;
+            else if(left>right) return false;
+            left += arr[i];
+            right -= arr[i];
         }
         
         return false;
