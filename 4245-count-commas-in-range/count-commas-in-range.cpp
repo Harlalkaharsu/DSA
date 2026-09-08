71ed7bcc14259818741f1f01455f58d9fd9f1ea8
Harshit Harlalka <harshitharlalka8118@gmail.com>
2026-09-08 09:53:52 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (84.49%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.cpp
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -1,8 +1,14 @@
 class Solution {
 public:
     int countCommas(int n) {
-        if(n==100000) return 99001;
-        if(n>=1000 && n<100000) return((n - 1000)+1);
-        else return 0;
+        int count = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (i >= 1000) {
+                count++;
+            }
+        }
+
+        return count;
     }
 };
